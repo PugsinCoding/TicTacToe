62ec1b3e738930d0c5859b8aab8a717f3989cba1
Chase S. <101994395+PugsinCoding@users.noreply.github.com>
2025-04-16 09:45:25 -0700

    git commit -m 'fixed win/scratch bug'

--- TicTacToe.js
+++ TicTacToe.js
@@ -15,6 +15,9 @@ let pinkPaw = [
     "Images/PP/Pink_Paw_DR.jpg",
     "Images/PP/Pink_Paw_DL.jpg"
 ]
+let index = [
+    '','','','','','','','',''
+];
 
 let bTurn = false;
 let pTurn = false;
@@ -23,11 +26,10 @@ function myFunction(){
     document.getElementById('board-base').ariaDisabled = false;
 }
 
-function bTrue(selectedDiv){
+function bTrue(selectedDiv, i){
     bTurn = true;
         let randNum = Math.floor((Math.random() * 5) + 1);
         let imgSrc = selectedDiv.querySelector('img');
-        console.log(randNum);
     switch(randNum){
         case 1:
             imgSrc.src = bluePaw[0];
@@ -49,14 +51,14 @@ function bTrue(selectedDiv){
     }
     selectedDiv.setAttribute("value", "clicked");
     selectedDiv.style.pointerEvents = 'none';
+    index[i] = 'b';
     bTurn = false;
 }
 
-function pTrue(selectedDiv){
+function pTrue(selectedDiv, i){
     pTurn = true;
     let randNum = Math.floor((Math.random() * 5) + 1);
     let imgSrc = selectedDiv.querySelector('img');
-    console.log(randNum);
 switch(randNum){
     case 1:
         imgSrc.src = pinkPaw[0];
@@ -78,20 +80,40 @@ switch(randNum){
 }
 selectedDiv.setAttribute("value", "clicked");
 selectedDiv.style.pointerEvents = 'none';
+index[i] = 'p';
 pTurn = false;
 }
 
 function whoseTurn(i){
     count++;
+    console.log(count);
     let selectedDiv = document.getElementById(i);
-    if(count % 2 == 1 && count <=9){
-        bTrue(selectedDiv);
+    let adjusted = i - 1;
+    if(count % 2 == 1 && count < 9){
+        if(count >=5){
+            let character = 'b';
+            bTrue(selectedDiv, adjusted);
+            win(character);
+        }
+        else{
+            bTrue(selectedDiv, adjusted);
+        }
     }
-    else if(count % 2 == 0 && count <= 9){
-        pTrue(selectedDiv);
+    else if(count % 2 == 0 && count < 9){
+        if(count >=5){
+            let character= 'p';
+            pTrue(selectedDiv, adjusted);
+            win(character);
+        }
+        else{
+            pTrue(selectedDiv, adjusted);
+        }
     }
     else{
-        scratchOrWin();
+        if(count == 9){
+            bTrue(selectedDiv, adjusted);
+            scratchOrWin();
+        }
     }
 }
 
@@ -101,9 +123,52 @@ function clearBoard(){
         selectDiv.querySelector("img").src = "Images/Blank_TTT_Sprite.jpg";
         selectDiv.style.pointerEvents = 'auto';
     }
+    for(let i = 0; i < 9; i++){
+        console.log(index[i]);
+        index[i] = '';
+    }
     count = 0;
+    document.getElementById("decision").innerHTML = "";
 }
 
-function win(){}
+function win(character){
+    let winner = false;
+    if((index[0] == character && index[1] == character && index[2] == character) || (index[3] == character && index[4] == character && index[5] == character) || (index[6] == character && index[7] == character && index[8] == character)){
+        winner = true;
+    }
+    else if((index[0] == character && index[3] == character && index[6] == character) || (index[1] == character && index[4] == character && index[7] == character) || (index[2] == character && index[5] == character && index[8] == character)){
+        winner = true;
+    }
+    else if((index[0] == character && index[4] == character && index[8] == character) || (index[6] == character && index[4] == character && index[2] == character)){
+        winner = true;
+    }
+    if(winner == true){
+        for(let i = 1; i < 10; i++){
+            let selectedDiv = document.getElementById(i);
+            selectedDiv.style.pointerEvents = 'none';
+        }
+        if(character == 'b'){
+            document.getElementById("decision").innerHTML = "Blue Paw Wins!";
+        }
+        else if(character == 'p'){
+            document.getElementById("decision").innerHTML = "Pink Paw Wins!";
+        }
+        return winner;
+    }
+    return winner;
+}
 
-function scratchOrWin(){}
+
+
+function scratchOrWin(){
+    if(win('b') == true){
+        document.getElementById("decision").innerHTML = "Blue Paw Wins!";
+    }
+    else if(win('p') == true){
+        console.log("Pink is Winner!");
+        document.getElementById("decision").innerHTML = "Pink Paw Wins!";
+    }
+    else{
+        document.getElementById("decision").innerHTML = "Scratch!";
+    }
+}
